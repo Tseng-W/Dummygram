8e01cea5af738fcea5657e5bf70a3d015c2fc977
Wen_T <48378751+Tseng-W@users.noreply.github.com>
2022-05-26 21:32:15 +0800

    init commit.
    1. pod init, add snaplit.
    2. Remove storyboard

---
+++ Dummygram/View/Lobby/LobbyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LobbyViewController.swift
+//  Dummygram
+//
+//  Created by 曾問 on 2022/5/24.
+//
+
+import UIKit
+
+class LobbyViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
